c5d70c7a18ace4aaf01ce1c0df6ce6a8e063fc82
DeBosOs <luc.hoogerwerf@hva.nl>
2024-10-16 10:06:20 +0200

    infinite canvas added
    Whitespace moet weg gehaald worden

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,59 @@
-// JS
+window.onload = function() {
+
+    const images = [
+        'images/kunstwerk_1.jpeg',
+        'images/kunstwerk_2.jpeg',
+        'images/kunstwerk_3.jpeg',
+    ];
+    
+    const viewportHeight = document.documentElement.clientHeight;
+
+    function createContainer() {
+        const section = document.createElement('section');
+        section.setAttribute('class', 'container');
+        return section;
+    }
+
+    function randomPlaatje() {
+        return images[Math.floor(Math.random() * images.length)];
+    }
+
+    function plaatjeRow() {
+        const section = createContainer();
+        
+        for (let index = 0; index < 7; index++) {
+            const img = randomPlaatje();
+            const imgEl = createImageElement(img);
+            section.appendChild(imgEl);
+        }
+        return section;
+    }
+    
+    const main = document.querySelector('main');
+
+    for (let index = 0; index < 5; index++) {
+        main.appendChild(plaatjeRow());
+    }
+    
+    function createImageElement(img) {
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', img);
+        imgEl.setAttribute('width', 200);
+        return imgEl;
+    }
+
+    window.addEventListener('scroll', function(event) {
+        const scrolledY = window.scrollY;
+        const pageHeight = document.documentElement.scrollHeight;
+
+        console.log(scrolledY);
+
+        const scrolledToEnd = (scrolledY + viewportHeight) >= pageHeight;
+
+        console.log(scrolledToEnd);
+        
+        if(scrolledToEnd){
+            main.appendChild(plaatjeRow());
+        }
+    });
+}
